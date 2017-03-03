51b0a0fcdec1640965d4a6536cec601e2560bd07
Joshua Tanner <mr-jt-96@hotmail.com>
2017-03-03 21:51:10 +1300

    Speech to text basic implementation

--- app/src/main/java/com/example/joshua/cura_tablet/MainActivity.java
+++ app/src/main/java/com/example/joshua/cura_tablet/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.joshua.cura_tablet;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,11 +8,29 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.Menu;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button scheduleButton;
     Button identifyButton;
+
+    TextView helloText;
+
+    ImageButton speechButton;
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +58,60 @@ public class MainActivity extends AppCompatActivity {
                 Identify();
             }
         });
+
+        helloText = (TextView) findViewById(R.id.Hello);
+
+        speechButton = (ImageButton) findViewById(R.id.MicButton);
+
+        speechButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+
+
+    }
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    helloText.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     public void Schedule() {
