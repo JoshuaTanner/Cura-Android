690b72a9b6c8cec96b105e92acd243589d27953a
Sena <love-diving@hotmail.com>
2017-03-15 19:53:26 +1300

    Updated Memories

--- app/src/main/java/com/example/joshua/cura_tablet/Memories.java
+++ app/src/main/java/com/example/joshua/cura_tablet/Memories.java
@@ -1,34 +1,157 @@
 package com.example.joshua.cura_tablet;
 
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Build;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.graphics.Typeface;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextClock;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class Memories extends AppCompatActivity {
+
+    ImageButton scheduleButton1;
+    ImageButton curaButton_family1;
+    ImageButton homeButton1;
+
+    TextClock txtClock_menu;
+    TextClock textDate_menu;
+
+    TextView helloText;
+
+
+    TextToSpeech TTS;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memories);
         layout();
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Init();
     }
 
+    public void Init() {
+        //Set fonts
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/KozGoPr6N-Heavy.otf");
+        //Clock Set fonts for clock
+        txtClock_menu = (TextClock) findViewById(R.id.txtClock_menuTime2);
+        txtClock_menu.setTypeface(typeFace);
+        //Date Set fonts for clock
+        textDate_menu = (TextClock) findViewById(R.id.txtClock_menuDate2);
+        textDate_menu.setTypeface(typeFace);
+        //setup button
+
+        scheduleButton1 = (ImageButton) findViewById(R.id.menubtn_schedule2);
+        scheduleButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Schedule();
+            }
+        });
+
+        homeButton1 = (ImageButton) findViewById(R.id.menubtn_home2);
+        homeButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Home();
+            }
+        });
+
+        //For speech functionallity
+        helloText = (TextView) findViewById(R.id.HelloText);
+
+        curaButton_family1 = (ImageButton) findViewById(R.id.menubtn_cura2);
+
+        curaButton_family1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                promptSpeechInput();
+            }
+        });
+
+        TTS = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+                if(i != TextToSpeech.ERROR){
+                    TTS.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+    }
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+
+        //TextToSpeech("How can I help you?");
+
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    helloText.setText(result.get(0));
+
+                    Log.i("LOG", helloText.toString());
+
+                    if(helloText.getText().equals("who is my son"))
+                    {
+                        Identify();
+                    }
+                    else if(helloText.getText().equals("what is next"))
+                    {
+                        NextTask();
+                    }
+                    else
+                    {
+                        TextToSpeech("I don't understand");
+                    }
+                }
+                break;
+            }
+
+        }
+    }
 
     public void layout()
     {
@@ -69,4 +192,50 @@ public class Memories extends AppCompatActivity {
         }
 
     }
+
+    public void TextToSpeech(String toSpeak){
+        Toast.makeText(getApplicationContext(),toSpeak,Toast.LENGTH_SHORT).show();
+        TTS.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+
+    public void NextTask() {
+        Log.i("LOG", "Accessing schedule");
+        TextToSpeech("Head to the dining room in 15 minutes for lunch");
+    }
+
+
+
+    public void Identify() {
+        Log.i("LOG", "Accessing identify");
+
+        Intent work = new Intent(Memories.this, Memories.class);
+        //work.putExtra("stopLat", response.getLatitude());
+
+        startActivity(work);
+        finish();
+    }
+
+    public void Schedule() {
+        Log.i("LOG", "Accessing schedulewindow");
+
+        Intent work = new Intent(Memories.this, Schedule.class);
+        //work.putExtra("stopLat", response.getLatitude());
+
+        startActivity(work);
+        finish();
+    }
+
+    public void Home() {
+        Log.i("LOG", "Accessing family tree");
+
+        Intent work = new Intent(Memories.this, MainActivity.class);
+        //work.putExtra("stopLat", response.getLatitude());
+
+        startActivity(work);
+        finish();
+    }
 }
+
+
+
